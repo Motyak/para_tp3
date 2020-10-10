d3fd0a89c6953fbb24e9acd2ffae7eaec1767e66
Anfosso Tommy <uapv1903668@achille.univ-avignon.fr>
2020-10-10 19:36:07 +0200

    tests pourris, jenleve \

--- src/exo1.cu
+++ src/exo1.cu
@@ -5,8 +5,10 @@
 #include <random>
 #include <chrono>
 
-#define N (2048*2048)
-#define THREAD_PER_BLOCK 512
+#define NB_OF_ELEM 16777216
+#define MAX_NB_THREADS 1024
+//nb de mesures pour le calcul du temps moyen d'execution
+#define SAMPLE_SIZE 10
 
 __global__ void add(int* a, int* b, int* c)
 {
@@ -14,28 +16,107 @@ __global__ void add(int* a, int* b, int* c)
     c[index] = a[index] + b[index];
 }
 
-void randomInts(int* a, int size)
+void randomInts(int* a, int nbOfElem)
 {
     std::random_device random;
-    for (int i = 0 ; i < size ; ++i)
+    for (int i = 0 ; i < nbOfElem ; ++i)
         a[i] = random() % 1000;
+}
+
+// affiche les premiers résultats de la somme
+void resultTest()
+{
+    printf("DEBUT TEST RESULTAT\n");
+
+    int *a, *b, *c;
+    int *d_a, *d_b, *d_c;
+    int size = NB_OF_ELEM * sizeof(int);
+
+    cudaMalloc((void**)&d_a, size);
+    cudaMalloc((void**)&d_b, size);
+    cudaMalloc((void**)&d_c, size);
+
+    a = (int*)malloc(size); randomInts(a, NB_OF_ELEM);
+    b = (int*)malloc(size); randomInts(b, NB_OF_ELEM);
+    c = (int*)malloc(size);
+
+    cudaMemcpy(d_a, a, size, cudaMemcpyHostToDevice);
+    cudaMemcpy(d_b, b, size, cudaMemcpyHostToDevice);
+
+    add<<<NB_OF_ELEM/MAX_NB_THREADS,MAX_NB_THREADS>>>(d_a, d_b, d_c);
+
+    cudaMemcpy(c, d_c, size, cudaMemcpyDeviceToHost);
+
+    // j'affiche que les 10 premieres pour verifier
+    for(int i = 0 ; i < 10 ; ++i)
+        printf("%d + %d = %d\n", a[i], b[i], c[i]);
+
+    free(a);        free(b);        free(c);
+    cudaFree(d_a);  cudaFree(d_b);  cudaFree(d_c);
+
+    printf("FIN TEST RESULTAT\n\n");
+}
+
+void speedTest(int nbOfBlocks, int nbThreadsPerBlock)
+{
+    printf("%d BLOCS ET %d THREADS/BLOC\n", nbOfBlocks, nbThreadsPerBlock);
+
+    int *a, *b, *c;
+    int *d_a, *d_b, *d_c;
+    int size = NB_OF_ELEM * sizeof(int);
+
+    cudaMalloc((void**)&d_a, size);
+    cudaMalloc((void**)&d_b, size);
+    cudaMalloc((void**)&d_c, size);
+
+    a = (int*)malloc(size); randomInts(a, NB_OF_ELEM);
+    b = (int*)malloc(size); randomInts(b, NB_OF_ELEM);
+    c = (int*)malloc(size);
+
+    cudaMemcpy(d_a, a, size, cudaMemcpyHostToDevice);
+    cudaMemcpy(d_b, b, size, cudaMemcpyHostToDevice);
+
+    int t_ns = 0;
+
+    for(int i = 1 ; i <= SAMPLE_SIZE ; ++i)
+    {
+        auto t1 = std::chrono::high_resolution_clock::now();
+        add<<<nbOfBlocks,nbThreadsPerBlock>>>(d_a, d_b, d_c);
+        auto t2 = std::chrono::high_resolution_clock::now();
+        t_ns += std::chrono::duration_cast<std::chrono::nanoseconds>(t2 - t1).count();
+    }
+
+    std::cout<<"done in "<<t_ns / SAMPLE_SIZE <<" ns"<<std::endl;
+
+    free(a);        free(b);        free(c);
+    cudaFree(d_a);  cudaFree(d_b);  cudaFree(d_c);
 }
 
 // nvcc -o bin/exo1 src/exo1.cu
 int main(void)
 {
+    // // test résultats conforme aux attentes
+    // resultTest();
+
+    // printf("DEBUT TESTS DE VITESSE\n");
+    // speedTest(NB_OF_ELEM/MAX_NB_THREADS, MAX_NB_THREADS);
+    // speedTest(NB_OF_ELEM, 1);
+    // speedTest(524288, 32);
+    // speedTest(NB_OF_ELEM/MAX_NB_THREADS, MAX_NB_THREADS);
+    // printf("FIN TESTS DE VITESSE\n");
+
     int *a, *b, *c;
     int *d_a, *d_b, *d_c;
-    int size = N * sizeof(int);
+    int size = NB_OF_ELEM * sizeof(int);
 
     cudaMalloc((void**)&d_a, size);
     cudaMalloc((void**)&d_b, size);
     cudaMalloc((void**)&d_c, size);
 
     a = (int*)malloc(size);
-    randomInts(a, N);
+    randomInts(a, NB_OF_ELEM);
     b = (int*)malloc(size);
-    randomInts(b, N);
+    randomInts(b, NB_OF_ELEM);
     c = (int*)malloc(size);
 
     cudaMemcpy(d_a, a, size, cudaMemcpyHostToDevice);
@@ -43,7 +124,7 @@ int main(void)
 
     auto t1 = std::chrono::high_resolution_clock::now();
 
-    add<<<N/THREAD_PER_BLOCK,THREAD_PER_BLOCK>>>(d_a, d_b, d_c);
+    add<<<16384,1024>>>(d_a, d_b, d_c);
 
     auto t2 = std::chrono::high_resolution_clock::now();
 
@@ -53,8 +134,8 @@ int main(void)
     for(int i = 0 ; i < 10 ; ++i)
         printf("%d + %d = %d\n", a[i], b[i], c[i]);
 
-    auto int_us = std::chrono::duration_cast<std::chrono::microseconds>(t2 - t1);
-    std::cout<<"done in "<<int_us.count()<<" μs"<<std::endl;
+    auto int_ns = std::chrono::duration_cast<std::chrono::nanoseconds>(t2 - t1);
+    std::cout<<"done in "<<int_ns.count()<<" ns"<<std::endl;
 
     free(a);
     free(b);
