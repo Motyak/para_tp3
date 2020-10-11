cb2b572460918ac02f09f61abac19824f3549f8c
Anfosso Tommy <uapv1903668@achille.univ-avignon.fr>
2020-10-11 15:42:04 +0200

    rapport ok

--- src/exo3.cu
+++ src/exo3.cu
@@ -5,9 +5,11 @@
 #include <random>
 #include <chrono>
 
-#define DIM 2048
-#define N (DIM*DIM)
-#define THREAD_PER_BLOCK 512
+#define NB_OF_ELEM 16777216
+#define DIM 4096
+#define MAX_NB_THREADS 1024
+//nb de mesures pour le calcul du temps moyen d'execution
+#define SAMPLE_SIZE 10
 
 __global__ void multiply(int* a, int* b, int* c)
 {
@@ -47,49 +49,86 @@ void printMatrix(int* m, int dim)
     printf("...\n\n");
 }
 
-// nvcc -o bin/exo3 src/exo3.cu
-int main(void)
+void resultTest()
 {
+    printf("DEBUT TEST RESULTAT\n");
+
     int *a, *b, *c;
     int *d_a, *d_b, *d_c;
-    int size = N * sizeof(int);
+    int size = NB_OF_ELEM * sizeof(int);
 
     cudaMalloc((void**)&d_a, size);
     cudaMalloc((void**)&d_b, size);
     cudaMalloc((void**)&d_c, size);
 
-    a = (int*)malloc(size);
-    randomInts(a, N);
-    b = (int*)malloc(size);
-    randomInts(b, N);
+    a = (int*)malloc(size); randomInts(a, NB_OF_ELEM);
+    b = (int*)malloc(size); randomInts(b, NB_OF_ELEM);
     c = (int*)malloc(size);
 
     cudaMemcpy(d_a, a, size, cudaMemcpyHostToDevice);
     cudaMemcpy(d_b, b, size, cudaMemcpyHostToDevice);
 
-    auto t1 = std::chrono::high_resolution_clock::now();
-
-    multiply<<<N/THREAD_PER_BLOCK,THREAD_PER_BLOCK>>>(d_a, d_b, d_c);
-
-    auto t2 = std::chrono::high_resolution_clock::now();
+    multiply<<<NB_OF_ELEM/MAX_NB_THREADS,MAX_NB_THREADS>>>(d_a, d_b, d_c);
 
     cudaMemcpy(c, d_c, size, cudaMemcpyDeviceToHost);
 
     // j'affiche les 3 premières lignes/col de la mat1, mat2
     // et mat résultat
-    printMatrix(a, DIM);
-    printMatrix(b, DIM);
-    printMatrix(c, DIM);
+    printMatrix(a, DIM);    printMatrix(b, DIM);    printMatrix(c, DIM);
 
-    auto int_us = std::chrono::duration_cast<std::chrono::microseconds>(t2 - t1);
-    std::cout<<"done in "<<int_us.count()<<" μs"<<std::endl;
+    free(a);        free(b);        free(c);
+    cudaFree(d_a);  cudaFree(d_b);  cudaFree(d_c);
 
-    free(a);
-    free(b);
-    free(c);
-    cudaFree(d_a); 
-    cudaFree(d_b); 
-    cudaFree(d_c);
+    printf("FIN TEST RESULTAT\n\n");
+}
+
+void speedTest(int nbOfBlocks, int nbThreadsPerBlock)
+{
+    printf("%d BLOCS ET %d THREADS/BLOC\n", nbOfBlocks, nbThreadsPerBlock);
+
+    int *a, *b, *c;
+    int *d_a, *d_b, *d_c;
+    int size = NB_OF_ELEM * sizeof(int);
+
+    cudaMalloc((void**)&d_a, size);
+    cudaMalloc((void**)&d_b, size);
+    cudaMalloc((void**)&d_c, size);
+
+    a = (int*)malloc(size); 
+    b = (int*)malloc(size); 
+    c = (int*)malloc(size);
+
+    int t_ns = 0;
+
+    for(int i = 1 ; i <= SAMPLE_SIZE ; ++i)
+    {
+        randomInts(a, NB_OF_ELEM);
+        randomInts(b, NB_OF_ELEM);
+        cudaMemcpy(d_a, a, size, cudaMemcpyHostToDevice);
+        cudaMemcpy(d_b, b, size, cudaMemcpyHostToDevice);
+        auto t1 = std::chrono::high_resolution_clock::now();
+        multiply<<<nbOfBlocks,nbThreadsPerBlock>>>(d_a, d_b, d_c);
+        auto t2 = std::chrono::high_resolution_clock::now();
+        std::cout<<std::chrono::duration_cast<std::chrono::nanoseconds>(t2 - t1).count()<<std::endl;
+        t_ns += std::chrono::duration_cast<std::chrono::nanoseconds>(t2 - t1).count();
+    }
+
+    std::cout<<"done in "<<t_ns / SAMPLE_SIZE <<" ns (in average)"<<std::endl<<std::endl;
+
+    free(a);        free(b);        free(c);
+    cudaFree(d_a);  cudaFree(d_b);  cudaFree(d_c);
+}
+
+// nvcc -o bin/exo3 src/exo3.cu
+int main(void)
+{
+    resultTest();
+
+    printf("DEBUT TESTS DE VITESSE\n");
+    speedTest(NB_OF_ELEM, 1);
+    speedTest(524288, 32);
+    speedTest(NB_OF_ELEM/MAX_NB_THREADS, MAX_NB_THREADS);
+    printf("FIN TESTS DE VITESSE\n");
 
     return 0;
 }
